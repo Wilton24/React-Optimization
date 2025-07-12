8ad81d4a37c30de2749916e320a74b91247eb73b
Wilton Batiquin <cbasher24@gmail.com>
2025-07-12 18:10:10 +0800

    Refactor counter components and remove unused state management

---
+++ src/components/Counter/ConfigureCounter.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+export default function ConfigureCounter({ handleSetCount }) {
+    const [enteredNumber, setEnteredNumber] = useState(0);
+    const [chosenCount, setChosenCount] = useState(0);
+
+    function handleSetClick() {
+        handleSetCount(enteredNumber);
+        setEnteredNumber(0);
+    }
+
+    function handleChange(event) {
+        setEnteredNumber(+event.target.value);
+    };
+
+    return (
+        <section id="configure-counter">
+            <h2>Set Counter</h2>
+            <input type="number" onChange={handleChange} value={enteredNumber} />
+            <button onClick={handleSetClick}>Set</button>
+        </section>
+    )
+}
